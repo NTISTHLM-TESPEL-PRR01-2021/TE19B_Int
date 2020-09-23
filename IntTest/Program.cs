506636aa6f0568c9706d9ff8b2e35d8c35639ec2
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-23 19:02:57 +0200

    Integer-demonstration

--- IntTest/Program.cs
+++ IntTest/Program.cs
@@ -2,11 +2,39 @@
 
 namespace IntTest
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
+      int hp;
+      int damage;
+
+      hp = 100;
+      hp = 200;
+      damage = 23;
+
+      hp = hp - damage;
+      hp -= damage;
+
+      Console.WriteLine(hp);
+
+
+      int points = 0;
+
+      points += 1;
+
+      points++;
+
+      Console.WriteLine("Du har fått " + points + " poäng");
+
+      if (points <= 4)
+      {
+        Console.WriteLine("HAI!");
+      }
+
+
+      Console.ReadLine();
     }
+  }
 }
